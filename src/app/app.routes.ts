78b7c44ff20fc29a8e6ff1b0e39d8b3cc49bd0ea
RonnyPumaricra <ronnylpqwerty@gmail.com>
2025-01-27 06:23:13 -0500

    Updated path tree

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InstrumentDisplayComponent } from './components/instrument-display/instrument-display.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: "",
+  //   component: undefined,
+  //   pathMatch: "full",
+  // },
+  {
+    path: ":instrument",
+    component: InstrumentDisplayComponent,
+  },
+];
