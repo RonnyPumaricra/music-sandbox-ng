0b6760b59e1e32b0021106f1b4f67e2ac88310d8
RonnyPumaricra <ronnylpqwerty@gmail.com>
2025-02-11 10:28:51 -0500

    Finished chords customization and update

--- src/app/components/chords-picker/chords-picker.component.ts
+++ src/app/components/chords-picker/chords-picker.component.ts
@@ -1,14 +1,16 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, output, signal } from '@angular/core';
 import { StoreService } from '../../services/store.service';
 import { MusicService } from '../../services/music.service';
 import { CrossComponent } from "../../svg/cross/cross.component";
 import { StoredChord } from '../../interfaces/stored-chord';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chords-picker',
   standalone: true,
   imports: [
     CrossComponent,
+    FormsModule,
 ],
   templateUrl: './chords-picker.component.html',
   styleUrl: './chords-picker.component.css'
@@ -16,6 +18,8 @@ import { StoredChord } from '../../interfaces/stored-chord';
 export class ChordsPickerComponent {
   storeService = inject(StoreService);
   musicService = inject(MusicService);
+
+  changeHighlightedNotes = output<number[]>();
 
   activeChordIndex = signal(-1);
   /* Handles invalid index (-1) */
@@ -43,6 +47,50 @@ export class ChordsPickerComponent {
       return storedChords;
     });
     this.activeChordIndex.set(-1);
+  }
+
+  chosenTriadIndex = 0;
+  chosenSeventhIndex = -1;
+
+  triadOptions = this.musicService.chords
+  .map((chord, index) => ({index, chord}))
+  .filter(({chord}) => chord.distribution.length == 3);
+  getSeventhOptions() {
+    const activeTriad = this.triadOptions[this.chosenTriadIndex].chord;
+    return this.musicService.chords
+    .map((chord, index) => ({index, chord}))
+    .filter(({chord}) => {
+      return chord.distribution[0] == activeTriad.distribution[0] &&
+            chord.distribution[1] == activeTriad.distribution[1] &&
+            chord.distribution[2] == activeTriad.distribution[2] &&
+            chord.distribution.length == 4;
+    });
+  };
+
+  updateChordIndex() {
+    let newChordIndex = this.chosenSeventhIndex == -1 ? this.chosenTriadIndex : this.chosenSeventhIndex;
+    const stChords = [...this.storeService.storedChords()];
+    stChords[this.activeChordIndex()] = {
+      chordIndex: newChordIndex,
+      distanceFromRoot: 0,
+    };
+    this.storeService.storedChords.set(stChords);
+  }
+
+  constructor() {
+    effect(() => {
+      let activeChord = this.activeChord();
+      if (activeChord == undefined) {
+        this.changeHighlightedNotes.emit([]);
+        return;
+      }
+      this.changeHighlightedNotes.emit(this.musicService.computeHighlightedChordNotes(
+        activeChord.distanceFromRoot,
+        activeChord.chordIndex,
+      ));
+    });
+
+    
   }
 
 }
