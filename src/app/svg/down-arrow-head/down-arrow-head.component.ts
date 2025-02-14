f11d59fdd2210a9c716fdc6ad318f23935596b05
RonnyPumaricra <ronnylpqwerty@gmail.com>
2025-02-14 11:37:11 -0500

    New down arrow icon

---
+++ src/app/svg/down-arrow-head/down-arrow-head.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'svg-down-arrow-head',
+  standalone: true,
+  imports: [],
+  templateUrl: './down-arrow-head.component.html',
+  styles: `
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`
+})
+export class DownArrowHeadComponent {
+}
