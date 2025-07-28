125b68cd0c5e2735d6f31c9b4b77f9b98f9a0b49
RonnyPumaricra <ronnylpqwerty@gmail.com>
2025-07-28 17:16:59 -0500

    Added picker of root note on the scale editor

--- src/app/components/scale-editor/scale-editor.component.ts
+++ src/app/components/scale-editor/scale-editor.component.ts
@@ -1,19 +1,24 @@
-import { Component, computed, inject, input, output } from '@angular/core';
+import { Component, computed, effect, inject, input, output, untracked } from '@angular/core';
 import { StoredKey } from '../../interfaces/stored-key';
 import { MusicService } from '../../services/music.service';
 import { FormsModule } from '@angular/forms';
+import { PickerComponent } from "../../svg/picker/picker.component";
+import { StoreService } from '../../services/store.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-scale-editor',
   standalone: true,
   imports: [
     FormsModule,
-  ],
+    PickerComponent
+],
   templateUrl: './scale-editor.component.html',
   styleUrl: './scale-editor.component.css'
 })
 export class ScaleEditorComponent {
   musicService = inject(MusicService);
+  storeService = inject(StoreService);
 
   key = input.required<StoredKey>();
 
@@ -27,8 +32,32 @@ export class ScaleEditorComponent {
   pickedModeIndex = 0;
 
   scaleOptions = this.musicService.scales.map(scale => {
-    return scale.leadingKey ?? scale.modes[0] as string
+    return scale.name
   });
 
+  waitingForRoot = false;
+
+  waitForRootNote() {
+    this.waitingForRoot = true;
+  }
+
+  // pickedNote = toSignal(this.storeService.pickedNote);
+
+  constructor() {
+    effect(() => {
+      let pickedNote = this.storeService.pickedNote();
+      console.table({pickedNote});
+      if (!this.waitingForRoot) return;
+      
+      const updatedKey = {
+        ...this.key(),
+        rootNoteIndex: pickedNote,
+      };
+
+      this.keySubmit.emit(updatedKey);
+
+      this.waitingForRoot = false;
+    });
+  }
 
 }
